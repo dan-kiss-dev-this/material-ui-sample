3b8b935e657d0618b51f5740fe6d4e972ed3333e
Dan Kiss <flowfast47@gmail.com>
2020-10-15 20:32:08 -0700

    add material ui samples

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from '@material-ui/core/Button'
+import ButtonGroup from '@material-ui/core/ButtonGroup'
+// import Accordion from '@material-ui/core/Accordion'
+import SaveIcon from '@material-ui/icons/Save';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+import { makeStyles, ThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import { orange, green } from '@material-ui/core/colors'
+import Container from '@material-ui/core/Container'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: orange[500]
+    },
+    secondary: {
+      main: green[500]
+    }
+  }
+})
+
+const useStyles = makeStyles({
+  root: {
+    background: 'linear-gradient(45deg, #333, #999)',
+    border: 0,
+    borderRadius: 15,
+    color: 'white',
+    padding: '0 30px'
+  }
+})
+
+function ButtonStyled() {
+  const classes = useStyles();
+  return <Button className={classes.root}>Test Styled Button</Button>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ThemeProvider theme={theme}>
+      <ButtonStyled />
+      <ButtonGroup>
+        <Button
+          endIcon={<SaveIcon />}
+          variant="contained"
+          color="primary"
+          size="large"
+          style={{
+            fontSize: 24
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Save
+      </Button>
+        <Button
+          endIcon={<DeleteIcon />}
+          variant="contained"
+          color="secondary"
+          size="large"
+          style={{
+            fontSize: 24
+          }}
+        >
+          Remove
+      </Button>
+
+      </ButtonGroup>
+
+      {/* <Accordion children={[1,2,3]}/> */}
+    </ThemeProvider>
   );
 }
 
